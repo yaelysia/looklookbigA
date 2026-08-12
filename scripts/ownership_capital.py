7c61c5243bd522b942191250958fac071d6675e2
zilin <126763947+yaelysia@users.noreply.github.com>
2026-08-12 09:11:22 +0800

    Checkpoint issue 50 shareholder count history and trends

--- scripts/ownership_capital.py
+++ scripts/ownership_capital.py
@@ -2,6 +2,7 @@ from ownership_capital_base import *
 import ownership_capital_base as _base
 import ownership_capital_top_holders as _top_holders
 import ownership_capital_institutional as _institutional
+import ownership_capital_shareholder_count as _shareholder_count
 
 
 fetch_top_holders = _top_holders.fetch_top_holders
@@ -14,8 +15,13 @@ normalize_institutional_holdings = _institutional.normalize_institutional_holdin
 INSTITUTIONAL_HISTORY_PERIODS = _institutional.INSTITUTIONAL_HISTORY_PERIODS
 INSTITUTIONAL_DETAIL_ENDPOINT = _institutional.INSTITUTIONAL_DETAIL_ENDPOINT
 
+fetch_shareholder_count = _shareholder_count.fetch_shareholder_count
+normalize_shareholder_count = _shareholder_count.normalize_shareholder_count
+SHAREHOLDER_COUNT_HISTORY_LIMIT = _shareholder_count.HISTORY_LIMIT
+
 
 def finalize_snapshot(snapshot_path, base, execution_mode):
     _base.finalize_snapshot(snapshot_path, base, execution_mode)
     _top_holders.extend_snapshot(snapshot_path, base, execution_mode)
     _institutional.extend_snapshot(snapshot_path, base, execution_mode)
+    _shareholder_count.extend_snapshot(snapshot_path, base, execution_mode)
