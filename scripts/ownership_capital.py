ad4f2a1c24fbd7f047f432869f5e5c99af79b1ff
zilin <126763947+yaelysia@users.noreply.github.com>
2026-08-12 08:07:14 +0800

    Wire institutional holdings into ownership snapshot

--- scripts/ownership_capital.py
+++ scripts/ownership_capital.py
@@ -1,6 +1,7 @@
 from ownership_capital_base import *
 import ownership_capital_base as _base
 import ownership_capital_top_holders as _top_holders
+import ownership_capital_institutional as _institutional
 
 
 fetch_top_holders = _top_holders.fetch_top_holders
@@ -8,7 +9,13 @@ normalize_top_holders = _top_holders.normalize_top_holders
 TOP_HOLDER_HISTORY_PERIODS = _top_holders.TOP_HOLDER_HISTORY_PERIODS
 DATA_CENTER_ENDPOINT = _top_holders.DATA_CENTER_ENDPOINT
 
+fetch_institutional_holdings = _institutional.fetch_institutional_holdings
+normalize_institutional_holdings = _institutional.normalize_institutional_holdings
+INSTITUTIONAL_HISTORY_PERIODS = _institutional.INSTITUTIONAL_HISTORY_PERIODS
+INSTITUTIONAL_DETAIL_ENDPOINT = _institutional.INSTITUTIONAL_DETAIL_ENDPOINT
+
 
 def finalize_snapshot(snapshot_path, base, execution_mode):
     _base.finalize_snapshot(snapshot_path, base, execution_mode)
     _top_holders.extend_snapshot(snapshot_path, base, execution_mode)
+    _institutional.extend_snapshot(snapshot_path, base, execution_mode)
